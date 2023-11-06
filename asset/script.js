5d549f74633092d9022ac32ece72f81bc0349a4a
PHAN MINH QUÂN <davidnguyen67@gmail.com>
2023-11-06 18:25:23 +0700

    check

--- asset/script.js
+++ asset/script.js
@@ -1,75 +1,83 @@
-$('.tombol').click(function() {
-    // animate content
-    $('.halaman').addClass('animate_content');
-    $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
+$('.tombol').click(function () {
+  // animate content
+  $('.halaman').addClass('animate_content');
+  $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
 
-    setTimeout(function() {
-        $('.halaman').removeClass('animate_content');
-    }, 3200);
+  setTimeout(function () {
+    $('.halaman').removeClass('animate_content');
+  }, 3200);
 
-    //remove fadeIn class after 1500ms
-    setTimeout(function() {
-        $('.halaman').removeClass('fadeIn');
-    }, 1100);
-
+  //remove fadeIn class after 1500ms
+  setTimeout(function () {
+    $('.halaman').removeClass('fadeIn');
+  }, 1100);
 });
 
+$('.tombol2').click(function () {
+  // animate content
+  $('.halaman').addClass('animate_content');
+  $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
 
-$('.tombol2').click(function() {
-    // animate content
-    $('.halaman').addClass('animate_content');
-    $('.dalemnya_halaman').fadeOut(100).delay(2800).fadeIn();
+  setTimeout(function () {
+    $('.halaman').removeClass('animate_content');
+  }, 3200);
 
-    setTimeout(function() {
-        $('.halaman').removeClass('animate_content');
-    }, 3200);
-
-    //remove fadeIn class after 1500ms
-    setTimeout(function() {
-        $('.halaman').removeClass('fadeIn');
-    }, 1100);
-
+  //remove fadeIn class after 1500ms
+  setTimeout(function () {
+    $('.halaman').removeClass('fadeIn');
+  }, 1100);
 });
 
+$('.kehome').click(function () {
+  setTimeout(function () {
+    $('.home').addClass('fadeIn');
+  }, 1100);
 
-$('.kehome').click(function() {
-    setTimeout(function() {
-        $('.home').addClass('fadeIn');
-    }, 1100);
-
-    PuterLagu();
+  PuterLagu();
 });
 
-
-$('.kehalaman2').click(function() {
-    setTimeout(function() {
-        $('.halaman2').addClass('fadeIn');
+$('.kehalaman2').click(function () {
+  let name = document.getElementById('nama').value;
+  name = name.toLowerCase().replace(/^\s+|\s+$/gm, '');
+  if (
+    name === 'nguyễn thị thu' ||
+    name === 'nguyễn thu' ||
+    name === 'nguyen thi thu' ||
+    name === 'nguyen thu'
+  ) {
+    setTimeout(function () {
+      $('.halaman2').addClass('fadeIn');
     }, 1100);
 
+    var i = 0;
+    var txt =
+      'Ribuan malam ku termenung sendiri, Menunggu seseorang untuk memelukku, Lewati ratusan mimpi tersimpan dalam sanubari, Sendiri di tengah kegelapan yang semu. Terimakasih engkau telah hadir untukku,\nmenemani kesepianku dan telah menjadi penerang dalam gelapku,  I Love You';
+    var speed = 150;
+
+    function JalaninTeks() {
+      if (i < txt.length) {
+        document.getElementById('tekss').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(JalaninTeks, speed);
+      }
+    }
+
+    function PuterLagu() {
+      var lagu = document.getElementById('lagu');
+      return lagu.paused ? lagu.play() : lagu.pause();
+    }
+
+    function Ambilnama() {
+      var x = document.getElementById('nama').value;
+      document.getElementById('tampilnama').innerHTML = x;
+    }
     JalaninTeks();
     PuterLagu();
     Ambilnama();
+  } else {
+    console.log(name);
+    alert('Please enter again');
+    document.getElementById('nama').value = '';
+    document.getElementById('nama').focus();
+  }
 });
-
-var i = 0;
-var txt = "Ribuan malam ku termenung sendiri, Menunggu seseorang untuk memelukku, Lewati ratusan mimpi tersimpan dalam sanubari, Sendiri di tengah kegelapan yang semu. Terimakasih engkau telah hadir untukku,\nmenemani kesepianku dan telah menjadi penerang dalam gelapku,  I Love You";
-var speed = 200;
-
-function JalaninTeks() {
-    if (i < txt.length) {
-        document.getElementById("tekss").innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(JalaninTeks, speed);
-    }
-};
-
-
-function PuterLagu() {
-    var lagu = document.getElementById("lagu");
-    return lagu.paused ? lagu.play() : lagu.pause();
-};
-
-function Ambilnama() {
-    var x = document.getElementById("nama").value;
-    document.getElementById("tampilnama").innerHTML = x;
-};
